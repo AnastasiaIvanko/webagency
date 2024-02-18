91d14cff073fd7460f407b2c7197f3272a6b2162
AnastasiaIvanko <140964142+AnastasiaIvanko@users.noreply.github.com>
2024-02-19 01:42:49 +0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    lightGallery(document.getElementById('gallery'), {
-        plugins: [lgThumbnail, lgFullscreen],
-        speed: 500
-    });
-});
